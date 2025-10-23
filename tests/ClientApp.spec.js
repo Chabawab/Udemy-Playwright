2884a973de7074f07e36b2e82f221347dd25d24e
Sechaba Msukwini <63983926+Chabawab@users.noreply.github.com>
2025-10-23 14:19:06 +0200

    Scripting done

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -102,26 +102,38 @@ test('@Webst Client App login', async ({ page }) => {
          break;
       }
    }
+   
+
+   await page.locator(".field [type='text']").nth(1).fill('377');
+   await page.locator(".field [type='text']").nth(2).fill('Payer');
+   await page.locator(".field [type='text']").nth(3).fill('C011XP');
+
+   // await page.pause();
  
-   // expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
-   // await page.locator(".action__submit").click();
-   // await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-   // const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-   // console.log(orderId);
+   await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+   await page.locator(".action__submit").click();
+
+   // Order confirmation page
+
+   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+   console.log(orderId);
  
-   // await page.locator("button[routerlink*='myorders']").click();
-   // await page.locator("tbody").waitFor();
-   // const rows = await page.locator("tbody tr");
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = await page.locator("tbody tr");
  
- 
-   // for (let i = 0; i < await rows.count(); ++i) {
-   //    const rowOrderId = await rows.nth(i).locator("th").textContent();
-   //    if (orderId.includes(rowOrderId)) {
-   //       await rows.nth(i).locator("button").first().click();
-   //       break;
-   //    }
-   // }
-   // const orderIdDetails = await page.locator(".col-text").textContent();
-   // expect(orderId.includes(orderIdDetails)).toBeTruthy();
+   //Dynamically searchs for the order ID and clicks the accociated button. #1 of the interview question
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
  
 });
