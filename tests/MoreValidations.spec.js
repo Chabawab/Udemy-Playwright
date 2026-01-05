3acf7cbf1a99d72cdc9da0358676ad43f5e557aa
Sechaba Msukwini <63983926+Chabawab@users.noreply.github.com>
2026-01-05 10:13:12 +0200

    WebAPIPart1

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -7,8 +7,22 @@ test('Popup Validations', async({page}) =>
     // await page.goBack();
     // await page.goForward();
 
+    // Checks if an element is hidden or visible
     await expect(page.locator('#displayed-text')).toBeVisible();
     await page.locator('#hide-textbox').click();
     await expect(page.locator('#displayed-text')).toBeHidden();
+
+    // JavaSript Ppoup. Handling Dialogue
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator('#confirmbtn').click();
+    
+    await page.locator('#mousehover').hover();
+
+    //Handling frames(iframe and frameset frame set)
+    const framespace = page.frameLocator('#courses-iframe');
+    framespace.locator("li a[href*='lifetime-access']:visible").click();
+    const textContent = await framespace.locator('.text h2').textContent();
+    console.log(textContent.split(' ')[1]);
+
 
 })
